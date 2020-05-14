4c78b35b7452d54b2066e9bc560152dfc8191644
Giancarlo Pacheco <giancarlo.pacheco@intl.att.com>
2020-05-14 01:36:00 -0400

    Add Send BTC view

--- breadwallet/WAC/ActionViewControllers/WACSendCoinViewController.swift
+++ breadwallet/WAC/ActionViewControllers/WACSendCoinViewController.swift
@@ -7,7 +7,56 @@
 //
 
 import UIKit
+import WacSDK
 
 class WACSendCoinViewController: WACActionViewController {
     
+    private var _pCode: String?
+    public var pCode: String? {
+        get {
+            return _pCode
+        }
+        set {
+            _pCode = newValue
+            self.pCodeTextField.text = newValue
+        }
+    }
+    private var _amount: String?
+    public var amount: String? {
+        get {
+            return _amount
+        }
+        set {
+            _amount = newValue
+            self.amountTextField.text = newValue
+        }
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var pCodeTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func checkCodeStatus(_ sender: Any) {
+        client?.checkCashCodeStatus(pCode!, completion: { (response: WacSDK.CashCodeStatusResponse) in
+            if (response.result == "error") {
+                let message = response.error?.message
+                self.showAlert(title: "Error", message: message!)
+            }
+            else {
+                self.showAlert(title: "Result", message: response.result)
+            }
+            self.actionCallback?.actiondDidComplete(action: .pCodeVerification)
+            self.clearViews()
+        })
+    }
+    
+    override func clearViews() {
+        self.pCodeTextField.text = ""
+        self.amountTextField.text = ""
+    }
 }
