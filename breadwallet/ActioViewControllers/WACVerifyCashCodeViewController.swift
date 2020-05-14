6413ada6ce9f5d70d957f9b24a96a37754545498
Giancarlo Pacheco <giancarlo.pacheco@intl.att.com>
2020-05-13 23:05:16 -0400

    organize project into a WAC folder, create parent class for page sheet view controllers, request verification code, confirm code and get data back

---
+++ breadwallet/ActioViewControllers/WACVerifyCashCodeViewController.swift
@@ -0,0 +1,58 @@
+// 
+//  WACVerifyCashCodeViewController.swift
+//
+//  Created by Giancarlo Pacheco on 5/13/20.
+//  Copyright © 2020 Breadwinner AG. All rights reserved.
+//
+
+import UIKit
+import WacSDK
+
+class WACVerifyCashCodeViewController: WACActionViewController {
+    
+    @IBOutlet weak var atmMachineTitleLabel: UILabel!
+    @IBOutlet weak var tokenTextView: UITextField!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    public var amount: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.confirmButton.isEnabled = false
+    }
+    
+    @IBAction func sendCashCode(_ sender: Any) {
+        self.view.endEditing(true)
+        client?.createCashCode((atm?.atmId)!, amount!, tokenTextView.text!, completion: { (response: CashCodeResponse) in
+            if response.result == "error" {
+                let message = response.error?.message
+                self.showAlert(title: "Error", message: message!)
+            }
+            self.view.hideAnimated()
+            self.actionCallback?.withdrawal(requested: (response.data?.items?[0])!)
+            self.actionCallback?.actiondDidComplete(action: .cashCodeVerification)
+            self.clearViews()
+        })
+    }
+    
+    override public func clearViews() {
+        super.clearViews()
+        self.tokenTextView.text = ""
+    }
+    
+    override func showView() {
+        super.showView()
+        self.listenForKeyboard = true
+    }
+    
+    @IBAction override func textDidChange(_ sender: Any) {
+        let code = self.tokenTextView.text
+        if (code != "") {
+            self.confirmButton.isEnabled = true
+        }
+        else {
+            self.confirmButton.isEnabled = false
+        }
+    }
+}
---
+++ breadwallet/WAC/ActionViewControllers/WACSendCoinViewController.swift
@@ -0,0 +1,13 @@
+// 
+//  WACSendCoinViewController.swift
+//
+//  Created by Giancarlo Pacheco on 5/13/20.
+//
+//  See the LICENSE file at the project root for license information.
+//
+
+import UIKit
+
+class WACSendCoinViewController: WACActionViewController {
+    
+}
