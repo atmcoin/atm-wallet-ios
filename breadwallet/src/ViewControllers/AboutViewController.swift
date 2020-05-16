b308b957f01174851408e3d566558e72d7893c7d
Gardner von Holt <gardner@wvh.com>
2020-05-16 10:10:13 -0500

    remove blog and fix twitter, reddit, and privacy

--- breadwallet/src/ViewControllers/AboutViewController.swift
+++ breadwallet/src/ViewControllers/AboutViewController.swift
@@ -15,7 +15,6 @@ class AboutViewController: UIViewController {
     private let logo = UIImageView(image: #imageLiteral(resourceName: "LogoCutout").withRenderingMode(.alwaysTemplate))
     private let logoBackground = MotionGradientView()
     private let walletID = WalletIDCell()
-    private let blog = AboutCell(text: S.About.blog)
     private let twitter = AboutCell(text: S.About.twitter)
     private let reddit = AboutCell(text: S.About.reddit)
     private let privacy = UIButton(type: .system)
@@ -33,7 +32,6 @@ class AboutViewController: UIViewController {
         view.addSubview(logoBackground)
         logoBackground.addSubview(logo)
         view.addSubview(walletID)
-        view.addSubview(blog)
         view.addSubview(twitter)
         view.addSubview(reddit)
         view.addSubview(privacy)
@@ -57,12 +55,8 @@ class AboutViewController: UIViewController {
             walletID.topAnchor.constraint(equalTo: logoBackground.bottomAnchor, constant: verticalMargin),
             walletID.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             walletID.trailingAnchor.constraint(equalTo: view.trailingAnchor) ])
-        blog.constrain([
-            blog.topAnchor.constraint(equalTo: walletID.bottomAnchor, constant: verticalMargin),
-            blog.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            blog.trailingAnchor.constraint(equalTo: view.trailingAnchor) ])
         twitter.constrain([
-            twitter.topAnchor.constraint(equalTo: blog.bottomAnchor, constant: verticalMargin),
+            twitter.topAnchor.constraint(equalTo: walletID.bottomAnchor, constant: verticalMargin),
             twitter.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             twitter.trailingAnchor.constraint(equalTo: view.trailingAnchor) ])
         reddit.constrain([
@@ -92,17 +86,14 @@ class AboutViewController: UIViewController {
     }
 
     private func setActions() {
-        blog.button.tap = strongify(self) { myself in
-            myself.presentURL(string: "https://brd.com/blog/")
-        }
         twitter.button.tap = strongify(self) { myself in
-            myself.presentURL(string: "https://twitter.com/brdhq")
+            myself.presentURL(string: "https://twitter.com/Coinsquare")
         }
         reddit.button.tap = strongify(self) { myself in
-            myself.presentURL(string: "https://reddit.com/r/brdapp/")
+            myself.presentURL(string: "https://reddit.com/r/coinsquare/")
         }
         privacy.tap = strongify(self) { myself in
-            myself.presentURL(string: "https://brd.com/privacy")
+            myself.presentURL(string: "https://coinsquare.com/privacy_policy")
         }
     }
 
