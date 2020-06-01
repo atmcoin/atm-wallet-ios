c76e308705f8e12f6a8e2542c6795fb87b267ae3
giancarlo <g1an@hotmail.com>
2020-06-01 16:52:37 -0400

    cleaning up some code

--- breadwallet/src/Extensions/MKMapView+Additions.swift
+++ breadwallet/src/Extensions/MKMapView+Additions.swift
@@ -43,5 +43,12 @@ extension MKMapView {
         label.font = font
         return label
     }
+    
+    func centerToLocation(_ location: CLLocation, regionRadius: CLLocationDistance = 1000) {
+      let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                latitudinalMeters: regionRadius,
+                                                longitudinalMeters: regionRadius)
+      setRegion(coordinateRegion, animated: true)
+    }
 
 }
